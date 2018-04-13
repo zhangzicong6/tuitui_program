63cb7ff08cdc4ce54a5f71c28ef3a23adff812a4
root <lionel@buzzy.cc>
2018-04-13 23:01:19 +0800

    提交

--- project/routes/index.js
+++ project/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/index/:index',function(req,res,next){
+router.get('/action/:index',function(req,res,next){
 	 res.render('action/index'+req.params.index);
 });
 
