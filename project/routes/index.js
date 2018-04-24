eaf4c8910ed6c663563f816a206177706d2490c7
root <lionel@buzzy.cc>
2018-04-24 18:35:41 +0800

    提交链接

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var taokouling_conf = require('../conf/taokouling.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,11 +8,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/action/:index',function(req,res,next){
-	 res.render('action/index'+req.params.index);
+	res.render('action/index',{index:req.params.index});
 });
+router.get('/zhangshi/:index',function(req,res,next){
+	res.render('good/zhangshi',{index:req.params.index});
+})
+router.get('/goods/:index',function(req,res,next){
+	res.render('good/index',{index:req.params.index});
+})
 
 router.get('/get_code',function(req,res,next){
-	res.send({code:'￥zEUZ0IYWcoe￥'})
+	var num = parseInt(req.query.num);
+	var token = taokouling_conf[''+num]
+	res.send({code:token});
 });
+
+
 
 module.exports = router;
