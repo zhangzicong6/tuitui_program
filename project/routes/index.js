41396f7d9649922e2d5cbb8e57d1c44aeb60b894
unknown <weblixin@163.com>
2018-05-30 14:36:44 +0800

    5.30淘口令更换

--- project/routes/index.js
+++ project/routes/index.js
@@ -10,12 +10,15 @@ router.get('/', function(req, res, next) {
 router.get('/action/:index',function(req,res,next){
 	res.render('action/index',{index:req.params.index});
 });
+
 router.get('/zhangshi/:index',function(req,res,next){
 	res.render('good/zhangshi',{index:req.params.index});
-})
+});
+
 router.get('/goods/:index',function(req,res,next){
 	res.render('good/index',{index:req.params.index});
-})
+});
+
 router.get('/step',function(req,res,next){
 	res.render('step/step');
 })
@@ -28,7 +31,6 @@ router.get('/get_code',function(req,res,next){
 	var token = taokouling_conf[num]
 	res.send({code:token});
 });
-
 
 
 module.exports = router;
