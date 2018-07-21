9fc5d32e4ca319463abcf0d738dee226e02c8384
root <lionel@buzzy.cc>
2018-07-21 14:12:57 +0800

    Ok

--- project/routes/adzone.js
+++ project/routes/adzone.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var VideoProgramModel = require('../model/VideoProgram.js');
 var AdzoneTaoModel = require('../model/AdzoneTao.js');
 var async = require('async');
+var mem = require('../util/mem.js')
 
 router.use('/get_video',function(req,res,next){
 	var pro = req.query.pro?req.query.pro:'test_program';
@@ -30,6 +31,21 @@ router.use('/get_video',function(req,res,next){
 			res.send(vp);
 	});
 	
+});
+
+router.use('/get_kouling',function(req,res,next){
+	mem.get('taobao_qun_kouling').then(function(value){
+		var arr;
+		if(value){
+			arr = value.split(',');
+		}else{
+			arr = require('../conf/taobao_qun.json').koulings;
+			mem.set('taobao_qun_kouling',arr.join(','),1000*60*5).then(function(){})
+		}
+		var index =parseInt(arr.length*Math.random())
+  		var c_mua = arr[index];
+  		res.send({status:'success',text:c_mua});
+	});
 });
 
 module.exports = router;
