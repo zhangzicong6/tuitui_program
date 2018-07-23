6fee766bee04c9b642b6a7385adeecd8a4a29cdf
root <lionel@buzzy.cc>
2018-07-23 09:52:26 +0800

    跨域支持

--- project/routes/adzone.js
+++ project/routes/adzone.js
@@ -34,10 +34,12 @@ router.use('/get_video',function(req,res,next){
 });
 
 router.use('/get_kouling',function(req,res,next){
+	res.header("Access-Control-Allow-Credentials", true)
 	res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
 	mem.get('taobao_qun_kouling').then(function(value){
 		var arr;
 		if(value){
