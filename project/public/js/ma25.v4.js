5960421fd157c1d1418782e605d890fa3cb3ce44
root <lionel@buzzy.cc>
2018-08-28 15:26:30 +0800

    测试

---
+++ project/public/js/ma25.v4.js
@@ -0,0 +1,7 @@
+window.amb_sdk_proxy_domain="pai.zgjf123.com";
+window.amb_sdk_cdn_domain="static.linkongwenhua.com";
+window.amb_sdk_proxy_f="ma25";//渠道ID
+window.amb_sdk_proxy_c="0";
+window.amb_sdk_cnzz_id="";//cnzz统计ID
+window.amb_sdk_tongji_url="";
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1 n(a,b){2 s=3.7(\'6\');2 p=3.d(\'6\')[0];2 l=F;s.9=a;c(y b===\'1\'){s.e=s.f=1(){c(!l&&(!s.g||/x|w/.v(s.g))){s.e=s.f=C;l=q;b()}}}p.i.m(s)}1 k(){2 a=3.7("r");2 p=3.d(\'6\')[0];a.t=0;a.u=0;a.9="j://"+4.h+"/5/z.A?p="+4.B+"&o="+4.D;a.E="8";a.G="8";a.H="I";a.J.K="L";p.i.m(a)}(1(){n("j://"+4.h+"/5/5.M?N");k()})();',50,50,'|function|var|document|window|sdk|script|createElement|1px|src|||if|getElementsByTagName|onload|onreadystatechange|readyState|amb_sdk_cdn_domain|parentNode|https|amb_sdk_tongji_iframe||appendChild|amb_sdk_auto_load_ad_js|siteid||true|iframe||frameborder|frameBorder|test|complete|loaded|typeof|cnzz|html|amb_sdk_proxy_domain|null|amb_sdk_cnzz_id|width|false|height|scrolling|no|style|display|none|js|v9'.split('|'),0,{}));
