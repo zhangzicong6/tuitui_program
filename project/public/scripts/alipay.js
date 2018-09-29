8530807c4653cb3d797c225b5dfe5f8f82ecdf4e
unknown <weblixin@163.com>
2018-09-29 17:38:07 +0800

    ok

--- project/public/scripts/alipay.js
+++ project/public/scripts/alipay.js
@@ -1,5 +1,5 @@
 function hrefs() {
-	window.history.pushState('forward', null, location.pathname);
+	window.history.pushState('forward', null, location.pathname + location.search);
 }
 if(window.history && window.history.pushState) {
 	window.onpopstate = function() {
