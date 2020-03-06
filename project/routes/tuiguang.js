c7291743e8f6a789c98a201d65c59ad65ee1a345
zhangzicong6 <zhangzicong6@gmail.com>
2020-03-06 22:24:54 +0800

    1

--- project/routes/tuiguang.js
+++ project/routes/tuiguang.js
@@ -17,11 +17,11 @@ function fullUrl(req) {
 
 function handleIpAndUa(ip, ua) {
     let uni_ip_h_ua =  (ip + ua.substring(0,ua.indexOf(')',ua.indexOf(')')+1)+1));
-    if(uni_ip_h_ua.indexOf('iPhone')!=-1){
+    /*if(uni_ip_h_ua.indexOf('iPhone')!=-1){
         let replace_start = uni_ip_h_ua.substring(0,uni_ip_h_ua.indexOf('(')+1);
         let replace_end =  uni_ip_h_ua.substring(uni_ip_h_ua.indexOf(')'))
         uni_ip_h_ua = replace_start+ 'iPhone' + replace_end
-    }
+    }*/
     return uni_ip_h_ua;
 }
 
